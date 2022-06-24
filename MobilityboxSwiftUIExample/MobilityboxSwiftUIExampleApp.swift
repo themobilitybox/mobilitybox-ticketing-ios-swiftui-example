da75060072504f1c53af50b5274b44c854eec793
tim.krusch <tim@vesputi.com>
2022-06-24 09:38:33 +0200

    Initial Commit

---
+++ MobilityboxSwiftUIExample/MobilityboxSwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobilityboxSwiftUIExampleApp.swift
+//  MobilityboxSwiftUIExample
+//
+//  Created by Tim Krusch on 24.06.22.
+//
+
+import SwiftUI
+
+@main
+struct MobilityboxSwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
